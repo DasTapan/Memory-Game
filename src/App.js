571f2a079a1b29d46cbe0ef67c4e2ce8ad6273e2
Tapan Das <tapands.das@gmail.com>
2023-03-22 11:02:00 +0530

    Add initial UI layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Header from "./components/Header/Header";
+import Container from "./components/CardContainer/Container";
+import Footer from "./components/Footer/Footer";
+import styles from "./App.module.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className={styles.App}>
+      <Header />
+      <Container />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import styles from "../styles/Header.module.css";
+
+function Header() {
+  return <div className={styles.header}></div>;
+}
+
+export default Header;
