9df29b700bb041cc91fa782ec796d317489798dc
Tapan Das <tapands.das@gmail.com>
2023-03-23 11:03:48 +0530

    Align primary structure

--- src/components/CardContainer/Container.js
+++ src/components/CardContainer/Container.js
@@ -1,7 +1,70 @@
 import styles from "../styles/Container.module.css";
 
 function Container() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <div className={styles.pic}></div>
+        <span className={styles.name}></span>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.pic}></div>
+        <span className={styles.name}></span>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.pic}></div>
+        <span className={styles.name}></span>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.pic}></div>
+        <span className={styles.name}></span>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.pic}></div>
+        <span className={styles.name}></span>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.pic}></div>
+        <span className={styles.name}></span>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.pic}></div>
+        <span className={styles.name}></span>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.pic}></div>
+        <span className={styles.name}></span>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.pic}></div>
+        <span className={styles.name}></span>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.pic}></div>
+        <span className={styles.name}></span>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.pic}></div>
+        <span className={styles.name}></span>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.pic}></div>
+        <span className={styles.name}></span>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.pic}></div>
+        <span className={styles.name}></span>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.pic}></div>
+        <span className={styles.name}></span>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.pic}></div>
+        <span className={styles.name}></span>
+      </div>
+    </div>
+  );
 }
 
 export default Container;
