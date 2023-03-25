e16759304791cc87d4a6557371fbbf149616c4b5
Tapan Das <tapands.das@gmail.com>
2023-03-25 10:44:08 +0530

    Register clicks on cards

--- src/components/CardContainer/Container.js
+++ src/components/CardContainer/Container.js
@@ -67,4 +67,20 @@ function Container() {
   );
 }
 
+function Name({ localClass, name }) {
+  return <span className={localClass}>{name}</span>;
+}
+
+function Container({ people }) {
+  const cards = people.map((person) => (
+    <Card key={person.id}>
+      <Pic localClass={picStyles.pic} imgSrc={person.imgUrl} />
+      <Name localClass={nameStyles.name} name={person.name} />
+    </Card>
+  ));
+
+  const displayCards = cards.slice(0, 15);
+  return <div className={styles.container}>{displayCards}</div>;
+}
+
 export default Container;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,13 @@
 import styles from "../styles/Header.module.css";
 
-function Header() {
-  return <div className={styles.header}></div>;
+function Header({ score, bestScore }) {
+  return (
+    <div className={styles.header}>
+      <span className="heading">Wild West Memory Game</span>
+      <BestScore count={score} />
+      <Score count={bestScore} />
+    </div>
+  );
 }
 
 export default Header;
